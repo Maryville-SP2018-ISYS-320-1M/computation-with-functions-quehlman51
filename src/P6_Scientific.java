c8473089f03f29ca560717e05a196d28876b6ca4
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:21:55 -0500

    Created method

--- src/P6_Scientific.java
+++ src/P6_Scientific.java
@@ -1,13 +1,16 @@
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P6_Scientific {
 
 	public static void main(String[] args) {
-		// REMINDER: Any print statements should only be in the main method
+		System.out.println(computeScientificValue( 6.5, 3));
+	}
+	public static double computeScientificValue( double number, double powerNumber ) {
+		return number * Math.pow(10, powerNumber);
 
 	}
 
