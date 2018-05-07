965f61b338c965a6f61f3323cbfcea6238f731d9
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:20:39 -0500

    Input expressions and made observations

--- src/P5_TestMathFunctionCalls.java
+++ src/P5_TestMathFunctionCalls.java
@@ -7,21 +7,21 @@
 /*
  5. Your predicted expression values:
  
- 	1. 1.6
- 	2. 36
- 	3. 7
- 	4. -5
- 	5. 5
- 	6. 8
- 	7. 17
- 	8. 14
+ 	1. 1.6   Correct
+ 	2. 36    Correct
+ 	3. 7     Correct
+ 	4. -5    Correct
+ 	5. 5     Correct
+ 	6. 8     Correct
+ 	7. 17    Correct
+ 	8. 14    Correct
   
   
  */
 public class P5_TestMathFunctionCalls {
 
 	public static void main(String[] args) {
-		System.out.println("REPLACE WITH YOUR EXPRESSION");
+		System.out.println(Math.max( 18 - 5, Math.ceil( 4.6 * 3 )));
 	}
 
 }
