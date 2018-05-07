643f79df392e87638caa6876114d3a8962bc6499
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:14:00 -0500

    Adjusted error and made comment.

--- src/P4_FixTempConverter.java
+++ src/P4_FixTempConverter.java
@@ -6,7 +6,9 @@
 
 /*
  What were the errors you found?
- 
+ Using double tempc will constantly cause the program to return 0.0. Remove it and
+ have the method return a double. Then simply have the print statement print the
+ method.
  
  */
 
@@ -14,12 +16,12 @@ public class P4_FixTempConverter {
 	public static void main(String[] args) {
 		double tempf = 98.6;
 		double tempc = 0.0;
-		ftoc(tempf, tempc);
-		System.out.println("Body temp in C is: " + tempc);
+		ftoc(tempf);
+		System.out.println("Body temp in C is: " + ftoc(tempf));
 	}
 
 	// converts Fahrenheit temperatures to Celsius
-	public static void ftoc(double tempf, double tempc) {
-	      tempc = (tempf - 32) * 5 / 9;
+	public static double ftoc(double tempf) {
+	      return (tempf - 32) * 5 / 9;
     }
 }
